d3eda0def50aa62ad089d5ca9b3139ad5303e09e
Wilbyl <85300573+Wilbyl@users.noreply.github.com>
2021-08-21 01:09:58 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,6 @@
+let n1 = parseInt(prompt ("digite um numer:"));
+
+    if (n1%2 === 0 ){alert("numero " +n1 + " é par")}
+    else if (n1%2=== 1){alert("seu numero " + n1 + " é impar")}
+     else  {alert("digite uma opção valida")};
+     
